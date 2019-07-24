deeac17744f1d579a3fab2c177e9351073443022
Mohamed Turki <hi@mturki.me>
2019-07-24 10:23:29 +0200

    Move tokens to env variables

--- run.js
+++ run.js
@@ -1,14 +1,17 @@
-#!/usr/bin/env node
-
-
+require('dotenv').config()
 const request = require('request-promise-native')
 
-const cmaToken = 'CFPAT-yUrjygNkXBByzw1fLQPO0w3wPfbqgcRkv0bTJBmu9UY'
-const cdaToken = 'dmr-ZnXfcBGqBYfI0SVUBfMTx5Caqv3yniM9-fluiw4'
+const cmaToken = process.env.CMA_TOKEN
+const cdaToken = process.env.CDA_TOKEN 
 const entryEndpoint = 'https://api.contentful.com/spaces/cow0tjjvljcp/entries/7LtY7jEaKBTMer7s2Xm4a1'
 const publishEndpoint = `${entryEndpoint}/published`
 const cdaEndpoint = 'https://cdn.contentful.com/spaces/cow0tjjvljcp/entries/7LtY7jEaKBTMer7s2Xm4a1'
-const numOfRuns = 10
+
+const msSince = (start) => {
+  const finish = process.hrtime(start)
+
+  return (finish[0] * 1000) + (finish[1] / 1e6);
+}
 
 async function sendRequest ({ method, url, token, version }) {
   const options = { 
@@ -26,40 +29,46 @@ async function sendRequest ({ method, url, token, version }) {
 }
 
 async function run () {
-  const entry = await sendRequest({
+  const r = await sendRequest({
     method: 'GET',
     url: entryEndpoint,
     token: cmaToken
   })
-  const version = JSON.parse(entry).sys.version
+  const entry = JSON.parse(r)
+  const version = entry.sys.version
+  const publishedCounter = entry.sys.publishedCounter
 
+  const startPublish = process.hrtime()
   await sendRequest({ 
     method: 'PUT',
     url: publishEndpoint,
     token: cmaToken,
     version
   })
-  const pubTime = Date.now()
-  let isPublished = false
-  let timeToPublish
+  let endPublish
 
-  while (!isPublished) {
+  while (true) {
     try {
       const r = await sendRequest({
         method: 'GET',
         url: cdaEndpoint,
         token: cdaToken
       })
+      const entry = JSON.parse(r)
 
-      timeToPublish = Date.now() - pubTime
-      isPublished = true
+      if (entry.sys.revision > publishedCounter) {
+        endPublish = msSince(startPublish)
+        break
+      }
     } catch (e) {
-      console.log(e)
+      if (e.statusCode !== 404) {
+        break
+      }
     }
   }
 
-  console.log(`${timeToPublish}ms`)
   await sendRequest({ method: 'DELETE', url: publishEndpoint, token: cmaToken, version: version+1 })
+  console.log(endPublish)
 }
 
 run()
