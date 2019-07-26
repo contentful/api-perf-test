eba36be7cc193d245695f9eb74e0949c585114dd
E.Azer Koçulu <azer@kodfabrik.com>
2019-07-26 13:50:05 +0200

    Add test for updating entry

--- bin/test-entry-update.js
+++ bin/test-entry-update.js
@@ -1,26 +1,31 @@
 require("dotenv").config()
 
-const { createNewEntry, publishEntry } = require("../lib/content")
+const { updateEntry, publishEntry } = require("../lib/content")
 const { waitUntilContentIsDelivered } = require("../lib/delivery")
 
 const cmaToken = process.env.CMA_TOKEN
 const spaceId = process.env.SPACE_ID
 const cdaToken = process.env.CDA_TOKEN
+const existingEntryId = process.env.EXISTING_ENTRY_ID
 
 run()
 
 async function run() {
   const publishStartedAt = process.hrtime()
 
-  // Create a new entry
+  // Update existing entry, get updated version number
   // Publish it
   // Send requests to CDA until published version is delivered
   // Print out the time that took
-  const [entryId, entryVersion] = await createNewEntry({ cmaToken, spaceId })
+  const entryVersion = await updateEntry({
+    cmaToken,
+    spaceId,
+    entryId: existingEntryId
+  })
 
-  await publishEntry({ cmaToken, spaceId, entryId })
+  await publishEntry({ cmaToken, spaceId, entryId: existingEntryId })
   await waitUntilContentIsDelivered({
-    entryId,
+    entryId: existingEntryId,
     expectedVersion: entryVersion,
     spaceId,
     cdaToken
