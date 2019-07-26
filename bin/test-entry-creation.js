ae9357b0c22da0580a5ccf87e7f7a8d3c491d981
E.Azer Koçulu <azer@kodfabrik.com>
2019-07-26 16:18:41 +0200

    Unprettify

--- bin/test-entry-creation.js
+++ bin/test-entry-creation.js
@@ -1,22 +1,22 @@
-require("dotenv").config()
+require('dotenv').config();
 
-const startTimer = require("../lib/timer")
-const { createEntry, publishEntry } = require("../lib/content")
-const { waitUntilContentIsDelivered } = require("../lib/delivery")
-const { simplePost, populateSimplePost } = require("../lib/content-structure")
+const startTimer = require('../lib/timer');
+const { createEntry, publishEntry } = require('../lib/content');
+const { waitUntilContentIsDelivered } = require('../lib/delivery');
+const { simplePost, populateSimplePost } = require('../lib/content-structure');
 
-const cmaToken = process.env.CMA_TOKEN
-const spaceId = process.env.SPACE_ID
-const cdaToken = process.env.CDA_TOKEN
+const cmaToken = process.env.CMA_TOKEN;
+const spaceId = process.env.SPACE_ID;
+const cdaToken = process.env.CDA_TOKEN;
 
-require.main === module && run({ cmaToken, cdaToken, spaceId })
+require.main === module && run({ cmaToken, cdaToken, spaceId });
 
 module.exports = {
   run
-}
+};
 
-async function run({ cmaToken, cdaToken, spaceId }) {
-  const endTimer = startTimer()
+async function run ({ cmaToken, cdaToken, spaceId }) {
+  const endTimer = startTimer();
 
   // Create a new entry
   // Publish it
@@ -27,15 +27,15 @@ async function run({ cmaToken, cdaToken, spaceId }) {
     spaceId,
     contentTypeId: simplePost.contentTypeId,
     populateContent: populateSimplePost
-  })
+  });
 
-  await publishEntry({ cmaToken, spaceId, entryId, version: entryVersion })
+  await publishEntry({ cmaToken, spaceId, entryId, version: entryVersion });
   await waitUntilContentIsDelivered({
     entryId,
     expectedRevision: 1,
     spaceId,
     cdaToken
-  })
+  });
 
-  console.log(endTimer())
+  console.log(endTimer());
 }
