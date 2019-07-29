e4e59472009edb75ed7cd2443c46bdbdfa5b464a
E.Azer Koçulu <azer@kodfabrik.com>
2019-07-29 13:26:02 +0200

    Clean up accidentally committed line

--- run.sh
+++ run.sh
@@ -3,6 +3,5 @@ namespace=CtfPerf
 for filename in bin/*
 do
     metricName=$(basename $filename | cut -f 1 -d '.' | sed -r 's/(^|_|-)([a-z])/\U\2/g')
-    echo $(node $filename)
     aws cloudwatch put-metric-data --metric-name $metricName --namespace $namespace --value $(node $filename) --unit Milliseconds --timestamp $(date -u +"%Y-%m-%dT%H:%M:%SZ") --region us-east-1
 done
