4964a58baa3662fc5562b7c349b154824c7ab7b9
E.Azer Koçulu <azer@kodfabrik.com>
2019-07-29 11:38:05 +0200

    Unprettify

--- bin/test-entry-creation.js
+++ bin/test-entry-creation.js
@@ -1,21 +1,21 @@
-require("dotenv").config()
+require('dotenv').config();
 
-const startTimer = require("../lib/timer")
-const { createEntry, publishEntry } = require("../lib/content")
-const { waitUntilContentIsDelivered } = require("../lib/delivery")
+const startTimer = require('../lib/timer');
+const { createEntry, publishEntry } = require('../lib/content');
+const { waitUntilContentIsDelivered } = require('../lib/delivery');
 
-const cmaToken = process.env.CMA_TOKEN
-const spaceId = process.env.SPACE_ID
-const cdaToken = process.env.CDA_TOKEN
+const cmaToken = process.env.CMA_TOKEN;
+const spaceId = process.env.SPACE_ID;
+const cdaToken = process.env.CDA_TOKEN;
 
-require.main === module && run({ cmaToken, cdaToken, spaceId })
+require.main === module && run({ cmaToken, cdaToken, spaceId });
 
 module.exports = {
   run
-}
+};
 
-async function run({ cmaToken, cdaToken, spaceId }) {
-  const endTimer = startTimer()
+async function run ({ cmaToken, cdaToken, spaceId }) {
+  const endTimer = startTimer();
 
   // Create a new entry
   // Publish it
@@ -24,27 +24,27 @@ async function run({ cmaToken, cdaToken, spaceId }) {
   const [entryId, entryVersion] = await createEntry({
     cmaToken,
     spaceId,
-    contentTypeId: "simpleContentType",
+    contentTypeId: 'simpleContentType',
     populateContent: populateSimpleEntry
-  })
+  });
 
-  await publishEntry({ cmaToken, spaceId, entryId, version: entryVersion })
+  await publishEntry({ cmaToken, spaceId, entryId, version: entryVersion });
   await waitUntilContentIsDelivered({
     entryId,
     expectedRevision: 1,
     spaceId,
     cdaToken
-  })
+  });
 
-  console.log(endTimer())
+  console.log(endTimer());
 }
 
-function populateSimpleEntry() {
+function populateSimpleEntry () {
   return {
     fields: {
       title: {
-        "en-US": Date.now().toString(36)
+        'en-US': Date.now().toString(36)
       }
     }
-  }
+  };
 }
